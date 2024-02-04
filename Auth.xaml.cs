523dd4345a543b48d60aaca890e327de15d6ba2f
FaRomik <147040323+Rommaaan@users.noreply.github.com>
2024-02-04 10:03:32 +0300

    Add files via upload

---
+++ Auth.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LoginISP;
+
+namespace LoginISP
+{
+    public partial class Auth : ContentControl
+    {
+
+        public Auth()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            var user = ClassDB.entity.User.Where(i => i.Login == txbLogin.Text && i.Password == psbPassword.Password).FirstOrDefault();
+
+            if (user.IdRole == 1)
+            {
+                ClassFrame.frameMenu.Content = new frameAdmin();
+            }
+
+            else if (user.IdRole == 2)
+            {
+                ClassFrame.frameMenu.Content = new frameUser();
+            }
+            else
+            {
+                MessageBox.Show("Ошибка! Неправильно введен логин или пароль");
+            }
+        }
+    }
+}
---
+++ ClassFrame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace LoginISP
+{
+    internal class ClassFrame
+    {
+        public static Frame frameAuth;
+        public static Frame frameAdmin;
+        public static Frame frameUser;
+        public static Frame frameMenu;
+    }
+}
